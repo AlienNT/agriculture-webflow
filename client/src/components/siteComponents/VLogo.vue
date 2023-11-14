b946e0d24ca360107a20afa97873914afc2a5aaa
Trysten M <nick.tristen22@gmail.com>
2023-11-15 00:46:54 +0200

    new (components) добавлен компонент лого

---
+++ client/src/components/siteComponents/VLogo.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import LogoImage from "./LogoImage.vue";
+</script>
+
+<template>
+<router-link class="logo" to="/">
+  <div class="logo-icon">
+    <LogoImage/>
+  </div>
+  <div class="logo-text">
+    Organick
+  </div>
+</router-link>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  height: 53px;
+  width: 36px;
+  gap: 4px;
+}
+.logo-text {
+  color: #274C5B;
+  font-family: Roboto,sans-serif;
+  font-size: 38px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+</style>
