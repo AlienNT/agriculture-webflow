a6207c599fa61266a309c6f0cf142a71dd587156
Trysten M <nick.tristen22@gmail.com>
2023-11-15 00:30:25 +0200

    new (components) добавлены основные компоненты

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import TheHeader from "./components/TheHeader.vue";
+import TheContent from "./components/TheContent.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
-  <router-view/>
+  <TheHeader/>
+  <TheContent/>
+  <TheFooter/>
 </template>
 
 <style lang="scss">
