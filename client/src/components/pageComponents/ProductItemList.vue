21c8579b73f1850c324731f6a6ee6698b3f2acc6
Trysten M <nick.tristen22@gmail.com>
2023-11-23 21:06:14 +0200

    new (pages/components) добавил компоненты отображения списка товаров и компонент секции со списком товаров домашней страницы

---
+++ client/src/components/pageComponents/ProductItemList.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ProductItemCard from "@/components/pageComponents/ProductItemCard.vue";
+
+defineProps({
+  listData: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+  <ul class="product-item-list">
+    <li
+        class="product-item"
+        v-for="(item, index) in listData"
+        :key="index"
+    >
+      <ProductItemCard
+          :card-data="{...item, readonly: true}"
+          class="product-card"
+      />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.product-item-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.product-item {
+  padding: 10px;
+  flex: 1 1 25%;
+}
+</style>
---
+++ client/src/components/pageComponents/home/HomeShop.vue
@@ -0,0 +1,70 @@
+<script setup>
+import VTitle from "@/components/siteComponents/VTitle.vue";
+import VCursive from "@/components/siteComponents/VCursive.vue";
+import VButton from "@/components/siteComponents/VButton.vue";
+import ProductItemList from "@/components/pageComponents/ProductItemList.vue";
+
+defineProps({
+  content: {
+    type: Object
+  }
+})
+
+function loadMore() {
+  console.log('loading...')
+}
+</script>
+
+<template>
+  <div class="home-shop container">
+    <VCursive
+        :value="content?.cursive"
+        class="cursive"
+    />
+    <VTitle
+        :value="content?.title"
+        class="title"
+    />
+    <ProductItemList
+        :list-data="content?.list"
+        class="products-list"
+    />
+    <VButton
+        :label="content?.buttonLabel"
+        @on-click="loadMore"
+        class="button"
+    />
+  </div>
+</template>
+
+<style scoped>
+.home-shop {
+  padding-top: 176px;
+  padding-bottom: 176px;
+}
+
+.cursive, .title {
+  text-align: center;
+}
+
+.cursive {
+  margin-bottom: 5px;
+}
+
+.title {
+  margin-bottom: 40px;
+  font-size: 50px;
+  font-weight: 800;
+}
+
+.products-list {
+  margin-bottom: 122px;
+  > * {
+    flex: 1 1 25%;
+  }
+}
+
+.button {
+  margin: 0 auto;
+}
+</style>
