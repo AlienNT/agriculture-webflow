69701edcae451044fc4e398afe9f37307309b576
Trysten M <nick.tristen22@gmail.com>
2023-11-26 00:32:21 +0200

    new (components) добавил компонент отображения списка каунтеров

---
+++ client/src/components/pageComponents/RoundCounterList.vue
@@ -0,0 +1,32 @@
+<script setup>
+import RoundCounter from "@/components/pageComponents/RoundCounter.vue";
+
+defineProps({
+  listData: {
+    type: Array
+  }
+})
+</script>
+
+<template>
+  <ul class="round-counters-list">
+    <li
+        v-for="(item, index) in listData"
+        :key="item?._id || index"
+    >
+      <RoundCounter
+          :value="item?.value"
+          :title="item?.title"
+      />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.round-counters-list {
+  display: flex;
+  gap: 62px;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
