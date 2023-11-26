4a042a041639eb6686e47f8adaf8808fe4b78c37
Trysten M <nick.tristen22@gmail.com>
2023-11-26 02:31:03 +0200

    new (components/pages) добавил компонент блога домашней страницы; добавил компонент шаблона для контента страниц

---
+++ client/src/components/pageComponents/home/HomeBlog.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import {buttonStyle, routeTo} from "@/helpers/buttonHelper.js";
+import BlogList from "@/components/pageComponents/BlogList.vue";
+import VCursive from "@/components/siteComponents/VCursive.vue";
+import VTitle from "@/components/siteComponents/VTitle.vue";
+import VButton from "@/components/siteComponents/VButton.vue";
+import PageContentTemplate from "@/components/templates/PageContentTemplate.vue";
+
+defineProps({
+  content: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+  <div class="home-blog">
+    <PageContentTemplate>
+      <template v-slot:cursive>
+        <VCursive
+            :value="content?.cursive"
+        />
+      </template>
+      <template v-slot:title>
+        <VTitle
+            :value="content?.title"
+        />
+      </template>
+      <template v-slot:button>
+        <VButton
+            :style-type="buttonStyle.WHITE"
+            :label="content?.buttonLabel"
+            @on-click="routeTo(content?.buttonRoute)"
+        />
+      </template>
+      <template v-slot:content>
+        <BlogList
+            :list-data="content?.blog?.list"
+        />
+      </template>
+    </PageContentTemplate>
+  </div>
+</template>
---
+++ client/src/components/templates/PageContentTemplate.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="template">
+    <div class="container template__container">
+      <div class="row template__title-row">
+        <div class="col template__col">
+          <slot name="cursive"/>
+          <slot name="title"/>
+        </div>
+        <div class="col template__col">
+          <slot name="button"/>
+        </div>
+      </div>
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$verticalPadding: 170px;
+
+.template {
+  padding-top: $verticalPadding;
+  padding-bottom: $verticalPadding;
+}
+
+.template__title-row {
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-bottom: 45px;
+  gap: 20px;
+}
+
+.template__col {
+  flex: 0 0 fit-content;
+
+  &:first-of-type {
+    max-width: 720px;
+  }
+}
+</style>
